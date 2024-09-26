dfe3e1afb14c4b592f5d8a86d277dad32f2ca269
aadivp <apanjabi17@gmail.com>
2024-09-26 18:57:15 -0400

    top down shooter

--- src/main/java/frc/robot/commands/ClimbCommands/ClimbTeleop.java
+++ src/main/java/frc/robot/commands/ClimbCommands/ClimbTeleop.java
@@ -8,7 +8,7 @@ import java.util.ResourceBundle.Control;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
-import frc.robot.Controls.ControlMap;
+import frc.robot.controls.ControlMap;
 import frc.robot.subsystems.Climb;
 
 public class ClimbTeleop extends Command {
